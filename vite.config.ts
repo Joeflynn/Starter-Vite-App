1f966bd96f87910c05a877b86b09665f2e4adb6c
Joeflynn <54875961+Joeflynn@users.noreply.github.com>
2024-02-13 16:50:25 -0800

    deploy prep

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/starter-vite-app/",
+  base: "/Starter-Vite-App/",
   plugins: [react()],
   resolve: {
     alias: {
